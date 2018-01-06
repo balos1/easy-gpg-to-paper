e3f9a55d13861613d79f1433d4dcbd80d961ef5a
Cody Balos <cjbalos@gmail.com>
2018-01-06 02:48:38 -0800

    update setup.py with weasyprint dependency and version bump

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='easy-gpg-to-paper',
-      version='0.1.0',
+      version='1.0.0',
       description='easy-gpg-to-paper aims to make exporting your secret gpg key to paper, and then restoring from paper, an easy and painless process.',
       author='Cody Balos',
       author_email='cjbalos@gmail.com',
       url='https://github.com/cojomojo/easy-gpg-to-paper',
       packages=['gpg2paper'],
-      install_requires=['Pillow>=4', 'qrcode>=5'],
+      install_requires=['Pillow>=4', 'qrcode>=5', 'weasyprint>=0.38,<=1.0'],
 )
